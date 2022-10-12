404620cc9d7f4a9caa91f07a11b6e5544c62d761
Igoryok8 <energymag@mail.ru>
2022-10-12 04:04:04 +0300

    Задания 3 и 4 выполнены

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Задание 1");
-
         int[] numbers = new int[3];
         numbers[0] = 1;
         numbers[1] = 2;
@@ -14,9 +15,6 @@ public class Main {
         System.out.println();
 
         double[] numbers1 = {1.57, 7.654, 9.986};
-        numbers1[0] = 1.57;
-        numbers1[1] = 7.654;
-        numbers1[2] = 9.986;
         for (int i = 0; i < 3; i++) {
             System.out.println(numbers1[i]);
         }
@@ -42,8 +40,11 @@ public class Main {
         numbers2[1] = 2;
         numbers2[2] = 3;
 
-        for (int i = 0; i < 3; i++) {
-            System.out.print(numbers2[i]+ ", ");
+        for (int i = 0; i < numbers2.length; i++) {
+            System.out.print(numbers2[i]);
+            if (i != numbers2.length - 1) {
+                System.out.print(", ");
+            }
         }
         System.out.println();
 
@@ -51,8 +52,11 @@ public class Main {
         numbers3[0] = 1.57;
         numbers3[1] = 7.654;
         numbers3[2] = 9.986;
-        for (int i = 0; i < 3; i++) {
-            System.out.print(numbers3[i]+ ", ");
+        for (int i = 0; i < numbers3.length; i++) {
+            System.out.print(numbers3[i]);
+            if (i != numbers3.length - 1) {
+                System.out.print(", ");
+            }
         }
         System.out.println();
 
@@ -65,9 +69,66 @@ public class Main {
         distance1[5] = 5;
         distance1[6] = 3;
 
-        for (int i = 0; i < 7; i++) {
-            System.out.print(distance1[i]+ ", ");
+        for (int i = 0; i < distance1.length; i++) {
+            System.out.print(distance1[i]);
+            if (i != distance1.length - 1) {
+                System.out.print(", ");
+            }
         }
+        System.out.println();
+
+        System.out.println("Задание 3");
+
+        int[] numbers4 = new int[3];
+        numbers4[0] = 1;
+        numbers4[1] = 2;
+        numbers4[2] = 3;
+
+        for (int i = 2; i >= 0; i--) {
+            System.out.print(numbers4[i]);
+            if (i != 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+
+        double[] numbers5 = {1.57, 7.654, 9.986};
+        numbers5[0] = 1.57;
+        numbers5[1] = 7.654;
+        numbers5[2] = 9.986;
+        for (int i = 2; i >= 0; i--) {
+            System.out.print(numbers5[i]);
+            if (i != 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+
+        int[] distance2 = new int[7];
+        distance2[0] = 12;
+        distance2[1] = 15;
+        distance2[2] = 10;
+        distance2[3] = 11;
+        distance2[4] = 14;
+        distance2[5] = 5;
+        distance2[6] = 3;
+
+        for (int i = 6; i >= 0; i--) {
+            System.out.print(distance2[i]);
+            if (i != 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+
+        System.out.println("Задание 4");
+        int[] numbers6 = {1, 2, 3};
+        for (int i = 0; i < numbers6.length; i++) {
+            if (numbers6[i] % 2 == 1) {
+                numbers6[i] += 1;
+            }
+        }
+        System.out.println(Arrays.toString(numbers6));
 
     }
 }
